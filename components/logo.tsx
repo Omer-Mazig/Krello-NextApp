7ca2e33b72d321602ad3dfc9b661ddf763618b7c
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-12 14:08:40 +0300

    modify lofo and metadata

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,7 +5,8 @@ export const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="Logo" height={64} width={64} />
+        <Image src="/logo.svg" alt="Logo" height={20} width={20} />
+        <p className="text-lg text-neutral-700">Krello</p>
       </div>
     </Link>
   );
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Krello",
+    description: "Collaborate, manage project and reach new productivity peacks.",
+}
