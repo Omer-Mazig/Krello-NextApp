04e8c35c177bfae4a800fa889aa073459eb44ec7
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-12 15:08:16 +0300

    setup clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("h-screen", inter.className)}>{children}</body>
     </html>
   );
 }
--- config/site.ts
+++ config/site.ts
@@ -2,3 +2,4 @@ export const siteConfig = {
     name: "Krello",
     description: "Collaborate, manage project and reach new productivity peacks.",
 }
+
