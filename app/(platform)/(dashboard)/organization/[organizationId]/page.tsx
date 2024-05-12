1d8e2d837cee3e4433394099af543677a8143f74
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-12 16:05:14 +0300

    added protedted routed

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,7 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  return <div>OrganizationIdPage</div>;
+};
+
+export default OrganizationIdPage;
--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,38 @@
 import {
   clerkMiddleware,
-  createRouteMatcher
+  createRouteMatcher,
+  redirectToSignIn
 } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isProtectedRoute = createRouteMatcher([
-  '/protected(.*)',
+  '/organization(.*)',
+  '/select-org(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
+  if (auth().userId && !isProtectedRoute(req)) {
+    let path ="/select-org"
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`
+    }
+
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({returnBackUrl: req.url})
+  }
+
+  if (auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
+
+  }
+  
 });
 
 export const config = {
