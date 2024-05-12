861cf6ba4491dc4fd3e583dfac59c401264fb93b
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-12 17:02:38 +0300

    added org-control and fix bug of url (navbar)

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-slate-100">
+    <div className="h-full bg-slate-100 flex flex-col">
       <Navbar />
-      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <main className="bg-slate-100 flex-grow">{children}</main>
       <Footer />
     </div>
   );
