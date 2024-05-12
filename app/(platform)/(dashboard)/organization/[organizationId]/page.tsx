647778c177d1624ce233a72e7b23ae6e249fd475
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-12 21:45:04 +0300

    conect prisma and db

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,30 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          className="bprder border-black p-1"
+          id="title"
+          name="title"
+          placeholder="Enter a title"
+          required
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
