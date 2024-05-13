bfb22ac84597cc7df607f385c9deac533cb9ade1
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-13 12:31:18 +0300

    added hint component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,15 @@
-import { db } from "@/lib/db";
+import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationIdPage = () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
