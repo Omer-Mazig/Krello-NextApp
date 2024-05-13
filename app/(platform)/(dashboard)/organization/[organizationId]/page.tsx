9f9db683a636549ce71f19aa244ca3f2eaa2136d
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-13 12:10:24 +0300

    added info component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,28 +1,10 @@
 import { db } from "@/lib/db";
+import { Info } from "./_components/info";
 
 const OrganizationIdPage = () => {
-  async function create(formData: FormData) {
-    "use server";
-
-    const title = formData.get("title") as string;
-    await db.board.create({
-      data: {
-        title,
-      },
-    });
-  }
-
   return (
-    <div>
-      <form action={create}>
-        <input
-          className="bprder border-black p-1"
-          id="title"
-          name="title"
-          placeholder="Enter a title"
-          required
-        />
-      </form>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   );
 };
