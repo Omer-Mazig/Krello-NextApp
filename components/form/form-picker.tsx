d66f91dd2e84167da636199f99feb8a709a67101
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-13 13:42:38 +0300

    hotlink unsplash

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 import { DEFAULT_IMAGES } from "@/constants/images";
+import Link from "next/link";
 
 interface FormPickerProps {
   id: string;
@@ -74,6 +75,13 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
               alt="Unsplash image"
               className="object-cover rounded-sm"
             />
+            <Link
+              href={image.links.html}
+              target="_blank"
+              className="opacity-0 group hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
+            >
+              {image.user.name}
+            </Link>
           </div>
         ))}
       </div>
