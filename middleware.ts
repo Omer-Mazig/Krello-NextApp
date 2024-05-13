979e9802b5f9ed0a02397048354cc9c2d08ec8c6
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-13 17:48:26 +0300

    fixed bugs related to data base (superbase)

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,19 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher,
-} from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-  '/organization(.*)',
-  '/select-org(.*)',
+  "/organization(.*)",
+  "/select-org(.*)",
+  "/board(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
   if (auth().userId && !isProtectedRoute(req)) {
-    let path ="/select-org"
+    let path = "/select-org";
 
     if (auth().orgId) {
-      path = `/organization/${auth().orgId}`
+      path = `/organization/${auth().orgId}`;
     }
 
     const orgSelection = new URL(path, req.url);
@@ -23,17 +21,19 @@ export default clerkMiddleware((auth, req) => {
   }
 
   if (!auth().userId && isProtectedRoute(req)) {
-    return auth().redirectToSignIn({returnBackUrl: req.url})
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
   }
 
-  if (auth().userId && !auth().orgId && req.nextUrl.pathname !== "/select-org") {
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
     const orgSelection = new URL("/select-org", req.url);
     return NextResponse.redirect(orgSelection);
-
   }
-  
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
