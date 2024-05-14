cc226fbe845c189685dd5a0156002578d43c1dff
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-05-14 07:56:33 +0300

    generate metadeta for org

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,13 @@
-import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
 
+import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return { title: startCase(orgSlug || "Organization") };
+}
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
